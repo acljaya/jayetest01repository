1b163a3a9f7622184ea2978d792f1b6acfa2f650
acljaya <72490240+acljaya@users.noreply.github.com>
2021-09-18 01:15:55 +0530

    updating  the int

--- myFirstJavacode.java
+++ myFirstJavacode.java
@@ -2,7 +2,7 @@ public class myFirstJavacode {
 
   public static void main(String[] args) {
 
-    int num = 29;
+    int num = 56415641;
     boolean flag = false;
     for (int i = 2; i <= num / 2; ++i) {
       // condition for nonprime number
